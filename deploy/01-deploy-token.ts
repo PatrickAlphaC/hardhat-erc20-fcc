269db490d598035d247825b5bc067c587cb002cd
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-05-24 16:26:25 -0400

    updated for ts

---
+++ deploy/01-deploy-token.ts
@@ -0,0 +1,31 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import verify from "../utils/verify"
+import { networkConfig, developmentChains, INITIAL_SUPPLY } from "../helper-hardhat-config"
+const deployToken: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+) {
+  // @ts-ignore
+  const { getNamedAccounts, deployments, network } = hre
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  const chainId: number = network.config.chainId!
+  const ourToken = await deploy("OurToken", {
+    from: deployer,
+    args: [INITIAL_SUPPLY],
+    log: true,
+    // we need to wait if on a live network so we can verify properly
+    waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
+  })
+  log(`ourToken deployed at ${ourToken.address}`)
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(ourToken.address, [INITIAL_SUPPLY])
+  }
+}
+
+export default deployToken
+deployToken.tags = ["all"]
