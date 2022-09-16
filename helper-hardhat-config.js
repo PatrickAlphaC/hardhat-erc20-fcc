bd62f4dfdb4f56d031ba1c925b7d1eabf8f9dac8
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-09-15 20:50:15 -0400

    update for goerli

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -3,10 +3,9 @@ const networkConfig = {
     name: "localhost",
   },
   // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
-  // Default one is ETH/USD contract on Kovan
-  42: {
-    name: "kovan",
-    ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+  5: {
+    name: "goerli",
+    ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
   },
 }
 const INITIAL_SUPPLY = "1000000000000000000000000"
